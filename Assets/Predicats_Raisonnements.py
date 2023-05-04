a180db3c5d9c97fea2daf45e4e58e68fef941d6f
simonrulof <simonber2001@gmail.com>
2023-05-04 15:42:58 +0200

    push mise en forme prédicats et raisonnements

--- Assets/Predicats_Raisonnements.py
+++ Assets/Predicats_Raisonnements.py
@@ -1,2 +1,30 @@
 def form_predicats_raisonnements(ACE_FILE_NAME):
-    return [], []
+    file = open(ACE_FILE_NAME, "r")
+
+    listNameSentences = []
+    listPredicates = []
+    listReasoning = []
+
+    i = 0
+
+    line = file.readline()
+    while line[:-1:] != '':
+        listNameSentences.append(line[:-2:])
+
+        listPredicates.append([])
+        line = file.readline()
+        while line[:-1:] != '':
+            listPredicates[i].append(line[:-1:])
+            line = file.readline()
+
+        listReasoning.append([])
+        line = file.readline()
+        while line[:-1:] != '':
+            listReasoning[i].append(line[:-1:])
+            line = file.readline()
+
+        file.readline()
+        line = file.readline()
+
+        i+= 1
+    return listNameSentences, listPredicates, listReasoning
--- FallaciesDetection.py
+++ FallaciesDetection.py
@@ -6,8 +6,7 @@ ACE_FILE_NAME = "ACE_SENTENCES"
 
 if __name__ == '__main__':
     # ETAPE 0 : METTRE EN FORME LES PREDICATS ET RAISONNEMENTS
-    listPredicates, listReasoning = form_predicats_raisonnements(ACE_FILE_NAME)
-
+    listNameSentences, listPredicates, listReasoning = form_predicats_raisonnements(ACE_FILE_NAME)
 
     #ETAPE 1 : TRANSFORMER LES PHRASES ACE EN DRS
     DRS_ListPredicates = transformACEToDRS(listPredicates)
